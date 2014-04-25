0cc6ada044d29f0ca7004da57c3d07f0c9bf4b92
Mike <rodrigues.mi@husky.neu.edu>
2014-04-24 20:03:57 -0400

    Some tests are implemented - still more to be implemented but it's more progress.

--- test/tests.js
+++ test/tests.js
@@ -5,10 +5,14 @@ var should = require('should'),
 
 var in_regularJson    = require('./JSON/regularJson'),
     in_nestedJson     = require('./JSON/nestedJson'),
+    in_nestedJson2    = require('./JSON/nestedJson2'),
     in_nestedQuotes   = require('./JSON/nestedQuotes'),
+    in_noData         = require('./JSON/noData.json'),
     out_regularJson   = '',
     out_nestedJson    = '',
-    out_nestedQuotes  = '';
+    out_nestedJson2   = '',
+    out_nestedQuotes  = '',
+    out_noData        = '';
 
 describe('json-2-csv', function() {
 
@@ -29,11 +33,25 @@ describe('json-2-csv', function() {
           });
         },
         function(callback) {
+          fs.readFile('test/CSV/nestedJson2.csv', function(err, data) {
+            if (err) callback(err);
+            out_nestedJson2 = data.toString();
+            callback(null);
+          });
+        },
+        function(callback) {
           fs.readFile('test/CSV/nestedQuotes.csv', function(err, data) {
             if (err) callback(err);
             out_nestedQuotes = data.toString();
             callback(null);
           });
+        },
+        function(callback) {
+          fs.readFile('test/CSV/noData.csv', function (err, data) {
+            if (err) callback(err);
+            out_noData = data.toString();
+            callback(null);
+          })
         }
       ],
       function(err, results) {
@@ -43,120 +61,38 @@ describe('json-2-csv', function() {
     );
   });
 
-  it('should convert JSON to CSV', function(done) {
+  it('should convert plain JSON to CSV', function(done) {
     json2csv.json2csv(in_regularJson, function(err, csv) {
       csv.should.equal(out_regularJson);
       done();
     })
   });
 
-  it('should parse nested JSON to CSV', function(done) {
-    json2csv(in_nestedJson, function(err, csv) {
+  it('should parse nested JSON to CSV - 1', function(done) {
+    json2csv.json2csv(in_nestedJson, function(err, csv) {
       csv.should.equal(out_nestedJson);
       done();
     })
   });
 
-  it('should parse data:{} to csv with only column title', function(done) {
-    json2csv({
-      data: {},
-      fields: ['carModel', 'price', 'color']
-    }, function(err, csv) {
-      csv.should.equal('"carModel","price","color"');
+  it('should parse nested JSON to CSV - 2', function(done) {
+    json2csv.json2csv(in_nestedJson2, function(err, csv) {
+      csv.should.equal(out_nestedJson2);
       done();
     })
   });
 
-  it('should parse data:[null] to csv with only column title', function(done) {
-    json2csv({
-      data: [null],
-      fields: ['carModel', 'price', 'color']
-    }, function(err, csv) {
-      csv.should.equal('"carModel","price","color"');
-      done();
-    })
-  });
-  it('should output only selected fields', function(done) {
-    json2csv({
-      data: _in,
-      fields: ['carModel', 'price']
-    }, function(err, csv) {
-      csv.should.equal(_out_selected);
+  it('should parse nested quotes in JSON to have quotes in CSV', function(done) {
+    json2csv.json2csv(in_nestedQuotes, function(err, csv) {
+      csv.should.equal(out_nestedQuotes);
       done();
     })
   });
 
-  it('should output not exist field with empty value', function(done) {
-    json2csv({
-      data: _in,
-      fields: ['first not exist field', 'carModel', 'price', 'not exist field', 'color']
-    }, function(err, csv) {
-      csv.should.equal(_out_withNotExistField);
+  it('should parse an empty array to an empty CSV', function(done) {
+    json2csv.json2csv(in_noData, function(err, csv) {
+      csv.should.equal(out_noData);
       done();
     })
   });
-
-  it('should output reversed order', function(done) {
-    json2csv({
-      data: _in,
-      fields: ['price', 'carModel']
-    }, function(err, csv) {
-      csv.should.equal(_out_reversed);
-      done();
-    })
-  });
-
-  it('should output a string', function(done) {
-    json2csv({
-      data: _in,
-      fields: ['carModel', 'price', 'color']
-    }, function(err, csv) {
-      csv.should.be.type('string');
-      done();
-    })
-  });
-
-  it('should escape quotes with double quotes', function(done) {
-    json2csv({
-      data: _in_quotes,
-      fields: ['a string']
-    }, function(err, csv) {
-      csv.should.equal(_out_quotes);
-      done();
-    })
-  });
-
-  it('should use a custom delimiter when \'del\' property is present', function(done) {
-    json2csv({
-      data: _in,
-      fields: ['carModel', 'price', 'color'],
-      del: '\t'
-    }, function(err, csv) {
-      csv.should.equal(_out_tsv);
-      done();
-    })
-  })
-
-  it('should name columns as specified in \'fieldNames\' property', function(done) {
-    json2csv({
-      data: _in,
-      fields: ['carModel', 'price'],
-      fieldNames: ['Car Model', 'Price USD']
-    }, function(err, csv) {
-      csv.should.equal(_out_fieldNames);
-      done();
-    })
-  })
-
-  it('should parse nested properties', function(done) {
-    json2csv({
-      data: _in_nested,
-      fields: ['carModel', 'priceRange.min', 'priceRange.max'],
-      fieldNames: ['Car Model', 'Min Price', 'Max Price']
-    }, function(err, csv) {
-      csv.should.equal(_out_nested);
-      done();
-    })
-  })
-
 });
