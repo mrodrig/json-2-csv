668b35ed0782e943225c42932904240a04e6b5ba
mrodrig <rodrigues.mi@husky.neu.edu>
2015-04-22 19:57:12 -0400

    Initial investigation/work on #22

---
+++ csv2json-test.js
@@ -0,0 +1,15 @@
+var converter = require('./lib/converter');
+
+var mycsv = '"header1",header2,"header3"\nrow1 column1,"row1 column2, with comma","row1 column3"\n"row2 column1","row2 column2","row2 column3"\n';
+var csv = 'header1,header2,header3\nrow1 column1,"row1 column2, with comma", row1 column3\nrow2 column1,row2 column2, row2 column3\n';
+
+converter.csv2json(csv, function (err, json) {
+    if (!err) {
+        return console.log(json);
+    }
+    throw err;
+}, {
+    DELIMITER : {
+        WRAP: '"'
+    }
+});
