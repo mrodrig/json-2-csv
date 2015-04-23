662de0b76ff35e89ce40e5166381cf37618b2b0a
mrodrig <rodrigues.mi@husky.neu.edu>
2015-04-23 19:37:32 -0400

    closer to the solution, still not 100% working; needs tests

--- csv2json-test.js
+++ csv2json-test.js
@@ -5,7 +5,7 @@ var csv = 'header1,header2,header3\n'
         + 'row1 column1,"row1 column2, with comma", row1 column3\n'
         + 'row2 column1,row2 column2, row2 column3\n';
 
-converter.csv2json(mycsv, function (err, json) {
+converter.csv2json(csv, function (err, json) {
     if (!err) {
         return console.log(json);
     }
