88983f2502934c73eedb0fe44d809d57135e3af6
mrodrig <rodrigues.mi@husky.neu.edu>
2015-04-23 17:50:16 -0400

    more investigation, started on a character by character parser since RegExps won't work

--- csv2json-test.js
+++ csv2json-test.js
@@ -1,9 +1,11 @@
 var converter = require('./lib/converter');
 
-var mycsv = '"header1",header2,"header3"\nrow1 column1,"row1 column2, with comma","row1 column3"\n"row2 column1","row2 column2","row2 column3"\n';
-var csv = 'header1,header2,header3\nrow1 column1,"row1 column2, with comma", row1 column3\nrow2 column1,row2 column2, row2 column3\n';
+var mycsv = 'header1,header2,"header3"\nrow1 column1,"row1 column2, with comma","row1 column3"\n"row2 column1","row2 column2","row2 column3"\n';
+var csv = 'header1,header2,header3\n'
+        + 'row1 column1,"row1 column2, with comma", row1 column3\n'
+        + 'row2 column1,row2 column2, row2 column3\n';
 
-converter.csv2json(csv, function (err, json) {
+converter.csv2json(mycsv, function (err, json) {
     if (!err) {
         return console.log(json);
     }
