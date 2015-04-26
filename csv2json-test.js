625b1fcd33024dd6172af02c852d8e8c4b6e948f
mrodrig <rodrigues.mi@husky.neu.edu>
2015-04-25 21:15:19 -0400

    closer to the solution, need to cover a few more cases - seems to be working well with my manual tests; needs tests added

--- csv2json-test.js
+++ csv2json-test.js
@@ -1,6 +1,9 @@
 var converter = require('./lib/converter');
 
-var mycsv = 'header1,header2,"header3"\nrow1 column1,"row1 column2, with comma","row1 column3"\n"row2 column1","row2 column2","row2 column3"\n';
+var mycsv = 'header1,header2,"header3"\n'
+        + 'row1 column1,"row1 column2, with comma","row1 column3"\n'
+        + '"row2 column1","row2 column2","row2 column3"\n';
+
 var csv = 'header1,header2,header3\n'
         + 'row1 column1,"row1 column2, with comma", row1 column3\n'
         + 'row2 column1,row2 column2, row2 column3\n';
