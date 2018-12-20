e12fd4acf074093339d76f53afb156179486cd76
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2018-12-19 23:42:31 -0500

    csv2json updated for preliminary RFC4180 compliance

--- csv_test.js
+++ csv_test.js
@@ -3,8 +3,11 @@ let converter = require('./src/converter');
 // let csv = 'field1,field2,field3,field4\n' +
 // '"test,string","["arrayval1","arrayval2"]",string123,"""test with quote"';
 
-let csv = 'eventId,values\n' +
-    '185919,"[""sure"",""thing"",""boss""]"\n';
+let csv = 'field1,field2,field3,field4\n' +
+    '"test,string","[""sure"",""thing"",""boss""]","{""test"":""123""}","""test with quote"\n' +
+    '"testingwithquote""","""test""","{""yello""}","""end"""\n' +
+    'false,null, nopest ,  nopes   \n' +
+    'undefined,null,false,1545278758108';
 
 let csv2jsonCallback = function (err, json) {
     if (err) throw err;
--- src/csv-2-json.js
+++ src/csv-2-json.js
@@ -5,9 +5,20 @@ let constants = require('./constants.json'),
     path = require('doc-path');
 
 const Csv2Json = function (options) {
-    const leadingWrapRegex = new RegExp('^' + options.delimiter.wrap),
-        endingWrapRegex = new RegExp(options.delimiter.wrap + '$'),
-        fieldValueWrapRegex = new RegExp('(^' + options.delimiter.wrap + '|' + options.delimiter.wrap + '$)');
+    const objectDetectionRegex = new RegExp('^(\\[|\\{).*(\\]|\\})$'),
+        escapedWrapDelimiterRegex = new RegExp(options.delimiter.wrap + options.delimiter.wrap, 'g');
+
+    /**
+     * Trims the header key, if specified by the user via the provided options
+     * @param headerKey
+     * @returns {*}
+     */
+    function trimHeaderKey(headerKey) {
+        if (options.trimHeaderFields) {
+            return headerKey.trim();
+        }
+        return headerKey;
+    }
 
     /**
      * Generate the JSON heading from the CSV
@@ -19,7 +30,7 @@ const Csv2Json = function (options) {
         let headerRow = params.lines[0];
         params.headerFields = splitLine(headerRow).map((headerKey, index) => {
             return {
-                value: options.trimHeaderFields ? headerKey.trim() : headerKey,
+                value: trimHeaderKey(headerKey),
                 index: index
             };
         });
@@ -75,30 +86,34 @@ const Csv2Json = function (options) {
 
             // If we reached the end of the line, add the remaining value
             if (index === lastCharacterIndex) {
-                splitLine.push(line.substring(stateVariables.startIndex, stateVariables.insideWrapDelimiter ? index : undefined));
+                splitLine.push(line.substring(stateVariables.startIndex));
             }
             // If the line starts with a wrap delimiter
             else if (character === options.delimiter.wrap && index === 0) {
                 stateVariables.insideWrapDelimiter = true;
                 stateVariables.parsingValue = true;
-                stateVariables.startIndex = index + 1;
+                stateVariables.startIndex = index;
             }
-
             // If we reached a wrap delimiter with a field delimiter after it (ie. *",)
             else if (character === options.delimiter.wrap && charAfter === options.delimiter.field) {
-                splitLine.push(line.substring(stateVariables.startIndex, index));
+                splitLine.push(line.substring(stateVariables.startIndex, index+1));
                 stateVariables.startIndex = index + 2; // next value starts after the field delimiter
                 stateVariables.insideWrapDelimiter = false;
                 stateVariables.parsingValue = false;
             }
             // If we reached a wrap delimiter with a field delimiter after it (ie. ,"*)
-            else if (character === options.delimiter.wrap && charBefore === options.delimiter.field) {
+            else if (character === options.delimiter.wrap && charBefore === options.delimiter.field && !stateVariables.insideWrapDelimiter) {
                 if (stateVariables.parsingValue) {
                     splitLine.push(line.substring(stateVariables.startIndex, index-1));
                 }
                 stateVariables.insideWrapDelimiter = true;
                 stateVariables.parsingValue = true;
-                stateVariables.startIndex = index + 1;
+                stateVariables.startIndex = index;
+            }
+            // // If we run into an escaped quote
+            else if (character === options.delimiter.wrap && charAfter === options.delimiter.wrap) {
+                index += 2;
+                continue;
             }
             // If we reached a field delimiter and are not inside the wrap delimiters (ie. *,*)
             else if (character === options.delimiter.field && charBefore !== options.delimiter.wrap &&
@@ -113,10 +128,6 @@ const Csv2Json = function (options) {
                 stateVariables.insideWrapDelimiter = false;
                 stateVariables.parsingValue = true;
                 stateVariables.startIndex = index + 1;
-            }
-            else if (character === options.delimiter.wrap && charAfter === options.delimiter.wrap && stateVariables.insideWrapDelimiter) {
-                line = line.slice(0, index) + line.slice(index+1); // Remove the current character from the line
-                lastCharacterIndex--; // Update the value since we removed a character
             }
             // Otherwise increment to the next character
             index++;
@@ -136,6 +147,11 @@ const Csv2Json = function (options) {
         return params;
     }
 
+    /**
+     * Retrieves the value for the record from the line at the provided key.
+     * @param line {String[]} split line values for the record
+     * @param key {Object} {index: Number, value: String}
+     */
     function retrieveRecordValueFromLine(line, key) {
         let value = null;
 
@@ -143,22 +159,35 @@ const Csv2Json = function (options) {
             value = line[key.index];
         }
 
+        // Perform any necessary value conversions on the record value
         return processRecordValue(value);
     }
 
+    /**
+     * Processes the record's value by parsing the data to ensure the CSV is
+     * converted to the JSON that created it.
+     * @param fieldValue {String}
+     * @returns {*}
+     */
     function processRecordValue(fieldValue) {
-        // Trim the value, if specified in the options
-        fieldValue = trimRecordValue(fieldValue);
-
-        // TODO: Handle case of null or undefined
         // If the value is an array representation, convert it
-        if (isArrayRepresentation(fieldValue)) {
-            fieldValue = convertArrayRepresentation(fieldValue);
+        let parsedJson = parseValue(fieldValue);
+        // If parsedJson is anything aside from an error, then we want to use the parsed value
+        // This allows us to interpret values like 'null' --> null, 'false' --> false
+        if (!_.isError(parsedJson)) {
+            fieldValue = parsedJson;
+        } else if (fieldValue === 'undefined') {
+            fieldValue = undefined;
         }
 
         return fieldValue;
     }
 
+    /**
+     * Trims the record value, if specified by the user via the options object
+     * @param fieldValue
+     * @returns {String|null}
+     */
     function trimRecordValue(fieldValue) {
         if (options.trimFieldValues && !_.isNull(fieldValue)) {
             return fieldValue.trim();
@@ -184,16 +213,31 @@ const Csv2Json = function (options) {
         }, {});
     }
 
+    /**
+     * Removes the outermost wrap delimiters from a value, if they are present
+     * Otherwise, the non-wrapped value is returned as is
+     * @param fieldValue
+     * @returns {String}
+     */
     function removeWrapDelimitersFromValue(fieldValue) {
         let firstChar = fieldValue[0],
             lastIndex = fieldValue.length - 1,
             lastChar = fieldValue[lastIndex];
         // If the field starts and ends with a wrap delimiter
-        // TODO: add check for containing comma (
         if (firstChar === options.delimiter.wrap && lastChar === options.delimiter.wrap) {
             return fieldValue.substr(1, lastIndex - 1);
         }
         return fieldValue;
+    }
+
+    /**
+     * Unescapes wrap delimiters by replacing duplicates with a single (eg. "" -> ")
+     * This is done in order to parse RFC 4180 compliant CSV back to JSON
+     * @param fieldValue
+     * @returns {String}
+     */
+    function unescapeWrapDelimiterInField(fieldValue) {
+        return fieldValue.replace(escapedWrapDelimiterRegex, options.delimiter.wrap);
     }
 
     /**
@@ -206,10 +250,11 @@ const Csv2Json = function (options) {
             // skip over empty lines
             if (!line) { return generatedJsonObjects; }
 
-            // TODO: remove the trim in future version, gets rid of whitespace in leading/ending values
             // Split the line using the given field delimiter after trimming whitespace
-            line = splitLine(line.trim()).map((fieldValue) => {
+            line = splitLine(line).map((fieldValue) => {
                 fieldValue = removeWrapDelimitersFromValue(fieldValue);
+                fieldValue = unescapeWrapDelimiterInField(fieldValue);
+                fieldValue = trimRecordValue(fieldValue);
 
                 return fieldValue;
             });
@@ -221,40 +266,17 @@ const Csv2Json = function (options) {
         return params;
     }
 
-    /** TODO: Refactor array and object representation handling **/
     /**
-     * Does the given value represent an array?
+     * Attempts to parse the provided value. If it is not parsable, then an error is returned
      * @param value
-     * @returns {boolean}
+     * @returns {*}
      */
-    function isArrayRepresentation(value) {
-        // Verify that there is a value and it starts with '[' and ends with ']'
-        return (value && /^\[.*\]$/.test(value));
-
-        // Future version:
-        // try {
-        //     return JSON.parse(value);;
-        // } catch (err) {
-        //     return false;
-        // }
-    }
-
-    /**
-     * Converts the value from a CSV 'array'
-     * @param arrayRepresentation
-     * @returns {Array}
-     */
-    function convertArrayRepresentation(arrayRepresentation) {
-        // Remove the '[' and ']' characters
-        arrayRepresentation = arrayRepresentation.replace(/(\[|\])/g, '');
-
-        // Split the arrayRepresentation into an array by the array delimiter
-        arrayRepresentation = arrayRepresentation.split(options.delimiter.array);
-
-        // Filter out non-empty strings
-        return _.filter(arrayRepresentation, function (value) {
-            return value;
-        });
+    function parseValue(value) {
+        try {
+            return JSON.parse(value);
+        } catch (err) {
+            return err;
+        }
     }
 
     /**
