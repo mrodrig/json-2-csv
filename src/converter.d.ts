b68556b488658b0016abd69bb4d37cd36bda51fc
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2019-03-22 22:54:32 -0400

    chore(release): 3.4.2

--- src/converter.d.ts
+++ src/converter.d.ts
@@ -74,6 +74,6 @@ export function json2csv(data: object[],
 export function json2csvAsync(data: object[], options?: IFullOptions): Promise<string>;
 
 export function csv2json(csv: string,
-                         callback: (err?: Error, data?: Array<unknown>) => void, options?: ISharedOptions): void;
+                         callback: (err?: Error, data?: unknown[]) => void, options?: ISharedOptions): void;
 
-export function csv2jsonAsync(csv: string, options?: ISharedOptions): Promise<Array<unknown>>;
+export function csv2jsonAsync(csv: string, options?: ISharedOptions): Promise<unknown[]>;
