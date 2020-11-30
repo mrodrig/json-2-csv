e6aa4a22e2a7bb000d9abd346cf8f9afe9ea18d8
Howard Dean Watts <howard@hdwatts.com>
2020-11-29 19:35:32 -0500

    Flatten support for >200k row arrays (#166)
    * flatten support for > 200k row arrays

---
+++ test/utilsTests.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "should" }]*/
+
+let should = require('should'),
+    utils = require('../src/utils');
+
+function runTests() {
+    describe('utils', () => {
+        describe('flatten()', () => {
+            it('should flatten a nested array', (done) => {
+                const nested = [[1], [2], [3], [4], [5]],
+                    expected = [1, 2, 3, 4, 5],
+                    result = utils.flatten(nested);
+
+                result.should.deepEqual(expected);
+                done();
+            });
+
+            it('should handle extremely large arrays', (done) => {
+                const nested = [],
+                    expected = [];
+                for (let a = 0; a < 234567; a++) {
+                    nested.push([a]);
+                    expected.push(a);
+                }
+
+                const result = utils.flatten(nested);
+                result.should.deepEqual(expected);
+                done();
+            });
+        });
+    });
+}
+
+module.exports = { runTests };
